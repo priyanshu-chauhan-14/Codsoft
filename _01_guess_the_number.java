707d59ede3aeda496899fe33f42aefb34e23daf2
Priyanshu <priyanshuchauhan95088@gmail.com>
2025-05-01 22:42:50 +0530

    final

--- _01_guess_the_number.java
+++ _01_guess_the_number.java
@@ -7,28 +7,28 @@ public class _01_guess_the_number{
     public static void main(String[] args) {
         Random rand = new Random();
         Scanner sc = new Scanner(System.in);
-        int num = rand.nextInt(100);
+        int num = rand.nextInt(100)+1;
         int key=0, count = 0;
-        do{
-            System.out.println("Guess the number");
+        while(true){
+            System.out.println("Guess the number from 1 - 100");
             try{
-                key = sc.nextInt();
+                    key = sc.nextInt();
+                    count++;
+                if(key==num){
+                    System.out.println("You won ! The number was "+num); 
+                    System.out.println("You guessed the number in "+count+" chance/chances");break;
+                }
+                else if (key>num){
+                    System.out.println("The number is less than "+key);
+                }
+                else if(key<num){
+                    System.out.println("The number is more than "+key);
+                }
             }
             catch(InputMismatchException e){
                 System.out.println("Enter a valid input "+e);
             }
-            if(key==num){
-                System.out.println("You won the number was "+num); 
-                System.out.println("You guessed the number in "+count+" chance/chances");break;
-            }
-            else if (key>num){
-                System.out.println("The number is less than "+key); ++count;
-            }
-            else if(key<num){
-                System.out.println("The number is more than "+key);
-                ++count;
-            }
-        }while(true);
+        }
         sc.close();
     }
 }
